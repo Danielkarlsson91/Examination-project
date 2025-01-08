12ac0f5f53483dbe3263618ce812f6aa05b9cbc6
Daniel Karlsson <daniel_karlsson91@hotmail.com>
2025-01-08 09:38:15 +0100

    Updated with some comments

--- client.py
+++ client.py
@@ -1,12 +1,15 @@
 import sys
 from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
 
+# Define the main window class that inherits from QMainWindow
 class MainWindow(QMainWindow):
     def __init__(self):
-        super().__init__()
+        super().__init__() # Call the parent class (QMainWindow) initializer
 
+        # Set the title of the main window
         self.setWindowTitle("My App")
 
+        # Create a QPushButton widget with the label "Press me!"
         button = QPushButton("Press me!")
         button.setCheckable(True)
         button.clicked.connect(self.the_button_was_clicked)
